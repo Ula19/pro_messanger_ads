d8f921a58f7a209f77e318e815a6ae414f692581
Ula19 <ulug.2000.2@gmail.com>
2026-02-02 16:30:43 +0500

    nothing

--- core/urls.py
+++ core/urls.py
@@ -24,10 +24,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/', include('apps.search_ads.urls')),
     path('api/', include('apps.billing.urls')),
     path('api/auth/', include('apps.account.urls')),
-    path('api/chat_post/', include('apps.chat_ads.urls')),
+    path('api/search_ads/', include('apps.search_ads.urls')),
+    path('api/chat_ads/', include('apps.chat_ads.urls')),
 ]
 
 if settings.DEBUG:
